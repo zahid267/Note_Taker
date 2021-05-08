79f30f9537505fac0966b0fab4c2c3b4be578b04
Muhammad Zahid <mrzahid@rogers.com>
2021-05-08 14:35:57 -0400

    Updating server.js file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const notes = require('./db/db');
 // Sets up the Express App
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
